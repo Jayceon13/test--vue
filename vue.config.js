d4762f589ffe3f6062b89cfa9eef50c3cdf9973f
Jayceon <jayceon310893@gmail.com>
2022-10-12 20:50:49 +0300

    test

--- vue.config.js
+++ vue.config.js
@@ -10,8 +10,12 @@ configureWebpack: {
     output: {
         filename: 'apps.js',
     },
-    options: {
-        hotReload: true,
-    }
+
 },
-})
+        chainWebpack: config => {
+            config.module.rule('vue').uses.delete('cache-loader');
+            config.module.rule('js').uses.delete('cache-loader');
+            config.module.rule('ts').uses.delete('cache-loader');
+            config.module.rule('tsx').uses.delete('cache-loader');
+}}
+)
