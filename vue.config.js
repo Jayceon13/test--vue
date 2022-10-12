83e24e8d59fdaf0650ca1138d63d5a4fc7fbdc9e
Jayceon <jayceon310893@gmail.com>
2022-10-12 19:18:03 +0300

    test

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    output: {
-        filename: 'apps.js',
-    },
   css: {
       extract: false,
   },
 configureWebpack: {
     optimization: {
         splitChunks: false,
+    },
+    output: {
+        filename: 'apps.js',
     },
 },
 })
